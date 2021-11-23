c421c9089a9a4d37b23a1350cb79437312b93885
josefdzbeta <jafb19@outlook.com>
2021-11-23 10:45:54 +0100

    casi terminado

--- js/validaciones.js
+++ js/validaciones.js
@@ -16,12 +16,14 @@ function iniciar(){
     console.log('Cargado JS')
     document.getElementById('spanMedia').onclick = calcularMedia
     document.getElementById('btnCero').onclick = ponerCero
-
+    document.getElementById('sCurso').onchange = asignaturas
+    
 }
 
 //Calculamos la media
 function calcularMedia(){
 
+    
 
     let num1, num2, num3, suma = 0, media
 
@@ -38,18 +40,46 @@ function calcularMedia(){
 
     console.log('Media: ' + media)
 
+    let span = document.getElementById('spanMedia')
+    span.textContent = media 
+    
 }
 
 //Poner los inputs tipo number a 0
 function ponerCero(){
 
-    document.getElementById('iCalculo1').value = "0" 
-    document.getElementById('iCalculo2').value = "0"
-    document.getElementById('iCalculo3').value = "0"
+    document.getElementById('iCalculo1').value = '0' 
+    document.getElementById('iCalculo2').value = '0'
+    document.getElementById('iCalculo3').value = '0'
+    document.getElementById('spanMedia').textContent = 'N/A'
 
 }
 
+function asignaturas(){
 
+    let p = document.getElementById('Clases');
 
+    //Creamos array de opciones 
+    let daw1  = ['Base de Datos','Sistemas Informaticos','Programacion','Lenguaje de Marcas']
+    let daw2 = ['DWEC', 'DWENS', 'DIW', 'DEAP']
 
+    //Crear y añadir select
+    let selectList = document.createElement("select");
+    p.appendChild(selectList);
 
+    if(document.getElementById('sCurso').value == '1DAW'){
+        for (let i = 0; i < daw1.length; i++) {
+            let option = document.createElement("option");
+            option.value = daw1[i];
+            option.text = daw1[i];
+            selectList.appendChild(option);
+        }
+    }else{
+        for (let i = 0; i < daw2.length; i++) {
+            let option = document.createElement("option");
+            option.value = daw2[i];
+            option.text = daw2[i];
+            selectList.appendChild(option);
+        }
+    }
+}
